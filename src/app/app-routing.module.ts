c6350487088f80d2a60ac1fb01437673ad042266
Vygovskii Oleg <vygovskii@yandex.ru>
2019-07-06 19:20:17 +0500

    lessons content markup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {LessonsComponent} from "./lessons/lessons.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/lessons', pathMatch: 'full'},
+  { path: 'lessons', component: LessonsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LessonsComponent } from './lessons/lessons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LessonsComponent
   ],
   imports: [
     BrowserModule,
